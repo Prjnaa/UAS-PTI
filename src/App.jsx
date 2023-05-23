0700d1ca50d06af1b4b40d74276353b106fa8fc4
BenJrx <128213708+BenJrx@users.noreply.github.com>
2023-05-23 16:06:09 +0700

    Friends

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom"
 import Login from './components/page-signin/Login'
 import Register from './components/page-signin/Regis'
 import Main from './components/page-main/Main'
+import Friend from './components/page-friends/src/Friend'
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Route path='/' element = {<Login />} />
       <Route path='/register' element = {<Register />} />
       <Route path='/main' element = {<Main/>} />
+      <Route path="/friend" element={<Friend/>}/>
     </Routes>
   )
 }
