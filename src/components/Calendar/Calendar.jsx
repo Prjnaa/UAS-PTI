b52bde777db186e746e5f88c3bb046c800ef7322
Mesayaaa <127949657+Mesayaaa@users.noreply.github.com>
2023-05-26 17:07:13 +0700

    update calendar

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -104,7 +104,7 @@ const Calendar = () => {
   };
 
   return (
-    <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }} className='bg-lgreen'>
+    <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }} className='bg-cust-1'>
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
