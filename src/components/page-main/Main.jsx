7e4f11e3eb06a342316d3d017e7e4437a42b30a2
Mesayaaa <127949657+Mesayaaa@users.noreply.github.com>
2023-05-28 22:54:21 +0700

    main.jsx

--- src/components/page-main/Main.jsx
+++ src/components/page-main/Main.jsx
@@ -11,7 +11,7 @@ import { storedUserState, userState } from "../currentUser.js";
 function Main() {
   console.log(userState.currentUser)
   return (
-    <div className="container-wrapper bg-cust-1 w-full grid grid-cols-12 py-3">
+    <div className="container-wrapper bg-cust-1 w-screen grid grid-cols-12 py-3">
       <motion.div
         initial={{ y: "-1000px" }}
         animate={{ y: 0 }}
