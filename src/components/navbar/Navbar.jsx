c6a7a5ea36c3918f5b98dfeec2a50f52669ec16f
Prjnaa <mr.vodoo05@gmail.com>
2023-05-29 16:34:08 +0700

    nyoba style

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ import "./nav.css";
 
 function Navbar() {
   return (
-    <div className="navbar-container sticky bottom-0 bg-cust-2 w-2/3 lg:h-24 md:h-20 h-14 shadow-box rounded-xl flex flex-row justify-around mx-auto">
+    <div className="navbar-container sticky bottom-0 bg-lightyellow w-2/3 lg:h-24 md:h-20 h-14 shadow-box rounded-xl flex flex-row justify-around mx-auto">
       <Navbutton imgURL={HomeSvg} navTo="/main" alt="Home" />
       <Navbutton imgURL={AboutUsSvg} navTo="/aboutUs" />
       <Logout imgURL={LogoutSvg} alt="Logout" />
--- src/components/page-main/Menubtn.jsx
+++ src/components/page-main/Menubtn.jsx
@@ -9,14 +9,14 @@ function Menubtn(props) {
   };
 
   return (
-    <div className="flex flex-col text-center mx-auto text-cust-1 mt-3">
+    <div className="flex flex-col text-center mx-auto text-cust-2 mt-3">
       <img
-        className="menubttn bg-cust-1 lg:h-28 lg:w-28 md:h-20 md:w-20 sm:h-18 sm:w-18 def:h-14 def:w-14 h-8 w-8 rounded-full transition duration-300 ease-in-out transform hover:scale-110"
+        className="menubttn bg-medblue lg:h-28 lg:w-28 md:h-20 md:w-20 sm:h-18 sm:w-18 def:h-14 def:w-14 h-8 w-8 rounded-full transition duration-300 ease-in-out transform hover:scale-110"
         src={props.imgURL}
         alt={props.ALT}
         onClick={handleClick}
       />
-      <p className="md:text-xl text-2xs font-semibold">{props.desc}</p>
+      <p className="md:text-xl text-2xs text-black font-semibold">{props.desc}</p>
     </div>
   );
 }
