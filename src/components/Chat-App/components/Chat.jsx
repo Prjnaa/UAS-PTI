cfb2c393757c4943f7d0dd71f8b40b4a8411b75a
Prjnaa <mr.vodoo05@gmail.com>
2023-05-30 07:43:55 +0700

    benerin fitur

--- src/components/Chat-App/components/Chat.jsx
+++ src/components/Chat-App/components/Chat.jsx
@@ -1,28 +1,31 @@
 import { useState, useEffect, useRef } from "react";
-import { addDoc, collection, serverTimestamp, onSnapshot, query, orderBy } from "firebase/firestore";
+import { addDoc, collection, serverTimestamp, onSnapshot, query, orderBy, doc, getDoc } from "firebase/firestore";
 import { db, auth } from "../../firebase";
-import '../../../index.css'
+import "../../../index.css";
+import { userState } from "../../currentUser";
 
 export default function Chat() {
   const [newMessage, setNewMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  
+
+  const currUser = userState.currentUser;
+
   const chatContainerRef = useRef(null);
   const messageRef = collection(db, "messages");
 
   useEffect(() => {
     const q = query(messageRef, orderBy("createdAt"));
-  
+
     const unsubscribe = onSnapshot(q, (snapShot) => {
       const _messages = snapShot.docs.map((doc) => {
         return {
           id: doc.id,
-          ...doc.data()
-        }
+          ...doc.data(),
+        };
       });
       setMessages(_messages);
     });
-  
+
     return () => {
       unsubscribe();
     };
@@ -34,24 +37,39 @@ export default function Chat() {
     }
   }, [messages]);
 
+  useEffect(() => {
+    const fetchUserName = async () => {
+      if (!auth.currentUser.displayName) {
+        const userDocRef = doc(db, "users", currUser); // Assuming users collection is named "users"
+        const userDocSnap = await getDoc(userDocRef);
+        if (userDocSnap.exists()) {
+          const userData = userDocSnap.data();
+          auth.currentUser.displayName = userData.userName;
+        }
+      }
+    };
+
+    fetchUserName();
+  }, [currUser]);
+
   const handleNewMessage = async (e) => {
     e.preventDefault();
-  
+
     if (newMessage === "") return;
-  
+
     await addDoc(messageRef, {
       text: newMessage,
       createdAt: serverTimestamp(),
       name: auth.currentUser.displayName,
       img: auth.currentUser.photoURL,
     });
-  
+
     setNewMessage("");
   };
 
   return (
     <div className="w-[80%] h-[80%] p-4 flex flex-col bg-cust-3 border border-gray-200 rounded-lg shadow mb-5 ">
-      <h1 className="font-semibold text-xl mb-3">Comunity Chat</h1>
+      <h1 className="font-semibold text-xl mb-3">Community Chat</h1>
       <div ref={chatContainerRef} className="flex flex-col flex-grow overflow-y-scroll space-y-2 mb-2">
         {messages.map((message) => (
           <div
@@ -60,9 +78,6 @@ export default function Chat() {
             }`}
             key={message.id}
           >
-            {message.name !== auth.currentUser.displayName && (
-              <img className="w-8 h-8 rounded-full" src={message.img} alt={message.name} />
-            )}
             <div
               className={`bg-cyan-100 rounded-lg p-2 ${
                 message.name === auth.currentUser.displayName ? " bg-cyan-100 text-black" : ""
@@ -84,7 +99,12 @@ export default function Chat() {
           onChange={(e) => setNewMessage(e.target.value)}
           placeholder="Type a new message..."
         />
-        <button className="bg-cust-4 w-32 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline hover:translate-y-[-5px] transition duration-300 ease-in-out" type="submit">Send</button>
+        <button
+          className="bg-cust-4 w-32 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline hover:translate-y-[-5px] transition duration-300 ease-in-out"
+          type="submit"
+        >
+          Send
+        </button>
       </form>
     </div>
   );
