2667e3579e576e6b64b0433feb35cc46cc70255e
Mesayaaa <127949657+Mesayaaa@users.noreply.github.com>
2023-06-13 15:06:57 +0700

    update

--- src/components/page-eventlist/Eventlist.jsx
+++ src/components/page-eventlist/Eventlist.jsx
@@ -122,7 +122,6 @@ const EventList = () => {
               <motion.h2
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5, delay: 0.1 }}
                 className="text-center text-lg font-semibold text-acc"
               >
                 {event.eventName}
